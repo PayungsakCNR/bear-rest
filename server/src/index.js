a04dd791832f678267e15d51faa28c34f8c7922c
PayungsakCNR <payungsak.k@cnr.psu.ac.th>
2018-04-27 00:12:35 +0700

    commit index.js
    commit index.js
    by PaO

---
+++ server/src/index.js
@@ -0,0 +1,34 @@
+var express = require('express'); 
+var app = express(); 
+var router = express.Router(); 
+var bodyParser = require('body-parser'); 
+var cors = require('cors');
+  
+var bears = [
+    { id: '1', name: 'Pao' },
+    { id: '2', name: 'Karn' },
+    { id: '3', name: 'Big' }
+]; 
+var last_bear_id = 4;
+  
+router.route('/bears') 
+    .get(function(req, res) {
+        res.send(bears);
+    })
+    .post(function(req, res) { 
+        var bear = {}; 
+        bear.name = req.body.name; 
+        bear.id = "" + (last_bear_id++);
+        bears.push(bear); 
+        res.json({ message: 'Bear created!' }); 
+    })
+router.route('/bears/:id')
+    .delete(function(req, res){
+        bears = bears.filter(b => b.id !== req.params.id)
+        res.json({ message: 'Bear deleted!' }); 
+    }) 
+
+app.use(cors());
+// all of our routes will be prefixed with /api 
+app.use('/api', bodyParser.json(), router); 
+app.listen(8000);
